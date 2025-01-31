e4e7e37164a4ba51620fa667a83a360189e8f5f7
Vitor de Oliveira <vitordeoliveirasv@gmail.com>
2025-01-31 09:22:15 -0300

    fix: update ssl mode to 'prefer' mode to create a postgres instance

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,6 +1,6 @@
 import postgres from 'postgres';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'prefer' });
 
 async function listInvoices() {
 	const data = await sql`
