50eb90464a3e18967f540478f30521865e3dd20c
Vitor de Oliveira <vitordeoliveirasv@gmail.com>
2025-01-31 15:09:58 -0300

    feat: add experimental partial prerendering to the dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from "../ui/dashboard/sidenav";
 
+export const experimental_ppr = true;
+
 // Defines a layout that will nest all pages in the app/dashboard
 export default function Layout({children} : {children: React.ReactNode}) {
     return (
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // incremental value allows to adopt PPR for specific routes
+    ppr: 'incremental'
+  }
 };
 
 export default nextConfig;
