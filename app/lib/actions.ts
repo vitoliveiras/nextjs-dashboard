bb3232d890ba6eb4ae20e84b9546ca48d4916367
Vitor de Oliveira <vitordeoliveirasv@gmail.com>
2025-02-03 17:15:24 -0300

    feat: add the invoice update

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -17,6 +17,8 @@ const FormSchema = z.object({
     date: z.string(),
 });
 
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
 // validate based to FormSchema omiting the fields id and date
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
@@ -41,3 +43,22 @@ export async function createInvoice(formData: FormData) {
     // redirect to invoices
     redirect('/dashboard/invoices');
 }
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+   
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -16,7 +16,8 @@ export function CreateInvoice() {
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href="/dashboard/invoices"
+      // accept the id prop
+      href={`/dashboard/invoices/${id}/edit`}
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
