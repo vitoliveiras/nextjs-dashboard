f4f10fe76489e7bd7e17a50b4338282b4c7aacc2
Vitor de Oliveira <vitordeoliveirasv@gmail.com>
2025-02-03 13:13:10 -0300

    feat: add the form type validation before inserting it into the database

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,33 @@
 'use server'; // mark all the exported function within the file as Server Actions
 
+// validation library
+import { z } from 'zod';
+
+// expected types from the database
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(), // coerce (change) from a string to a number
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+// validate based to FormSchema omiting the fields id and date
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
+    // validate the types
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
-        status: formData.get('status')
-    };
+        status: formData.get('status'),
+    });
+    // storage amount in cents
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
 
-    console.log(rawFormData);
+    console.log(customerId);
+    console.log(amountInCents);
+    console.log(status);
+    console.log(date);
 }
