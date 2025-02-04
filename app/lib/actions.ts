ab716d4bd298651573a2a8b1cb6e39c348876b69
Vitor de Oliveira <vitordeoliveirasv@gmail.com>
2025-02-04 16:47:53 -0300

    fix: add prevState parameter to the updateInvoice function in actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -81,7 +81,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
     const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
