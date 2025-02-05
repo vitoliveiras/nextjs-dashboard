a8656a26df7a7cd339e96944f92ec28d316a4bfd
Vitor de Oliveira <vitordeoliveirasv@gmail.com>
2025-02-05 16:56:23 -0300

    feat: display the error message in the login form

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -71,7 +71,14 @@ export default function LoginForm() {
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
         <div className="flex h-8 items-end space-x-1">
-          {/* Add form errors here */}
+          {
+            errorMessage && (
+              <>
+                <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+                <p className="text-sm text-red-500">{errorMessage}</p>
+              </>
+            )
+          }
         </div>
       </div>
     </form>
