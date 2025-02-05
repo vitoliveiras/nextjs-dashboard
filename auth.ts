d9a7af6ad376dabfaa5f674e8523c786873055d9
Vitor de Oliveira <vitordeoliveirasv@gmail.com>
2025-02-05 11:21:14 -0300

    feat: add function to get an user by id

--- auth.ts
+++ auth.ts
@@ -3,7 +3,23 @@ import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
+import postgres from 'postgres';
 
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'prefer' });
+
+async function getUser(email: string): Promise<User | undefined> {
+    try {
+        const user = await sql<User[]>`
+        SELECT * FROM users
+        WHERE email=${email}
+        `;
+        return user[0];
+    } catch (error) {
+        console.error('Failed to fetch user: ', error);
+        throw new Error('Failed to fetch user.');
+    }
+}
 export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
     // only add credentials provider (login with a username and password)
