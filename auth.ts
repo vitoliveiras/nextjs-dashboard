b8de87b781d24722458f397be7451b468aa0fc86
Vitor de Oliveira <vitordeoliveirasv@gmail.com>
2025-02-05 11:17:05 -0300

    feat: add the initial logic to handle the authentication logic

--- auth.ts
+++ auth.ts
@@ -1,8 +1,21 @@
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
+import { z } from 'zod';
+import type { User } from '@/app/lib/definitions';
 
 export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
-    providers: [Credentials({})], // only add credentials provider (login with a username and password)
+    // only add credentials provider (login with a username and password)
+    providers: [
+        Credentials({
+            async authorize(credentials: Partial<Record<string | number, unknown>>, request: Request): Promise<User | null> {
+                // this function can be used to handle the authentication logic (similarly to Server Action)
+                const parsedCredentials = z
+                    .object({ email: z.string().email(), password: z.string().min(6) })
+                    .safeParse(credentials);
+
+                    return null;
+            }
+        })],
 });
