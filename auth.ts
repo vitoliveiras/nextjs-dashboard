885756b2e33b521eda64f83a967c3af3e46cb953
Vitor de Oliveira <vitordeoliveirasv@gmail.com>
2025-02-05 11:37:01 -0300

    feat: add the password validation to the authentication logic

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import { authConfig } from './auth.config';
 import { z } from 'zod';
 import type { User } from '@/app/lib/definitions';
 import postgres from 'postgres';
-
+import bcrypt from 'bcrypt';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'prefer' });
 
@@ -20,17 +20,39 @@ async function getUser(email: string): Promise<User | undefined> {
         throw new Error('Failed to fetch user.');
     }
 }
+
 export const { auth, signIn, signOut } = NextAuth({
     ...authConfig,
     // only add credentials provider (login with a username and password)
     providers: [
         Credentials({
-            async authorize(credentials: Partial<Record<string | number, unknown>>, request: Request): Promise<User | null> {
+            async authorize(credentials) {
                 // this function can be used to handle the authentication logic (similarly to Server Action)
                 const parsedCredentials = z
                     .object({ email: z.string().email(), password: z.string().min(6) })
                     .safeParse(credentials);
 
+                    if (parsedCredentials.success) {
+                        // successful zod data validation
+                        const { email, password } = parsedCredentials.data;
+                        const user = await getUser(email);
+                        if (!user) {
+                            // email not found
+                            return null;
+                        }
+
+                        // if user was found, decrypt and validate your password
+                        const passwordMatch = await bcrypt.compare(password, user.password);
+
+                        if (passwordMatch) {
+                            // user authenticated
+                            return user;
+                        }
+
+                        console.log('Invalid credentials');
+                        // prevents the user from logging in
+                        return null;
+                    }
                     return null;
             }
         })],
